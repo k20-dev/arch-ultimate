1ba268290b1be7428348e5efd6d35625323a9a09
Daniel Plata <danielplata79@gmail.com>
2021-04-09 01:22:01 +0200

    added xbindkeys

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -4,15 +4,17 @@
 compton & 
 # WAL
 wal -i /home/daniel19/media/wallpapers/background02.jpg -a 90 &
+
 # KEYBOARD ES
-setxkbmap -layout es &
+# setxkbmap -layout es &
+
 # TOUCHPAD
-synclient rightedge=2200 &
-synclient ClickPad=1 &
-synclient TapButton1=1 &
-synclient MaxSpeed=1 &
-synclient VertEdgeScroll=1 &
-synclient HorizTwoFingerScroll=0 &
-synclient VertTwoFingerScroll=0 &
-synclient HorizEdgeScroll=1 &
-synclient BottomEdge=300 &
+# synclient rightedge=2200 &
+# synclient ClickPad=1 &
+# synclient TapButton1=1 &
+# synclient MaxSpeed=1 &
+# synclient VertEdgeScroll=1 &
+# synclient HorizTwoFingerScroll=0 &
+# synclient VertTwoFingerScroll=0 &
+# synclient HorizEdgeScroll=1 &
+# synclient BottomEdge=300 &
