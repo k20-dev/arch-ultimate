b4fea19c2c2fc8aa419f018d0c8dde8689453296
Daniel Plata <danielplata79@gmail.com>
2024-12-30 21:10:06 -0500

    QT5CT & QT6CT dotfiles, updated Fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -21,7 +21,10 @@
 # Initialize Oh-My-Posh
 #oh-my-posh --init --shell fish --config ~/.poshthemes/plague.omp.json | source
 #oh-my-posh --init --shell fish --config ~/.poshthemes/atomicBit.omp.json | source
-oh-my-posh --init --shell fish --config ~/.poshthemes/amro.omp.json | source
+#oh-my-posh --init --shell fish --config ~/.poshthemes/amro.omp.json | source
+#oh-my-posh --init --shell fish --config ~/.poshthemes/catppuccin_mocha.omp.json | source
+#oh-my-posh --init --shell fish --config ~/.poshthemes/lambdageneration.omp.json | source
+oh-my-posh --init --shell fish --config ~/.poshthemes/hul10.omp.json | source
 
 
 # ALIASES
@@ -30,7 +33,8 @@ alias 	gh	   "cd ~/"
 alias 	codes  "cd ~/code"
 
 # Current project
-alias	app		"cd ~/code/p5/p5-bezier"	 	
+alias	app  "cd ~/code/p5/p5-bezier"	 	
+alias	chat "cd ~/code/reactjschat"
 
 # Files  : Edit files
 alias   lvimrc   "cat ~/.vimrc"
@@ -39,9 +43,13 @@ alias 	lfish  "cat ~/.config/fish/config.fish"
 alias 	efish  "vim ~/.config/fish/config.fish"
 
 # Git Token
-alias 	gittoken	"echo git push https://danielplata79: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@github.com/danielplata79/p5-galaxy"
-
+alias 	gittoken "echo git push https://danielplata79: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@github.com/danielplata79/reactjschat"
+# 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
 # List Aliases
 
 # SHORTCUTS
 alias 	rr 	   "ranger" 
+
+# Screens
+alias d2off "xrandr --output DisplayPort-1 --off"
+alias d2on "xrandr --output DisplayPort-1 --mode 1920x1080 --pos 2560x0 --rotate normal --gamma 0.85:0.85:0.85"
