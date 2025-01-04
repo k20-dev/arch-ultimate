f82fb4a5160b8a96031853d8798f809f4fdb017f
Daniel Plata <danielplata79@gmail.com>
2025-01-04 12:12:16 -0500

    Updated Fish Shell Scripts

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -21,10 +21,8 @@
 # Initialize Oh-My-Posh
 #oh-my-posh --init --shell fish --config ~/.poshthemes/plague.omp.json | source
 #oh-my-posh --init --shell fish --config ~/.poshthemes/atomicBit.omp.json | source
-#oh-my-posh --init --shell fish --config ~/.poshthemes/amro.omp.json | source
-#oh-my-posh --init --shell fish --config ~/.poshthemes/catppuccin_mocha.omp.json | source
-#oh-my-posh --init --shell fish --config ~/.poshthemes/lambdageneration.omp.json | source
-oh-my-posh --init --shell fish --config ~/.poshthemes/hul10.omp.json | source
+#oh-my-posh init fish | source
+oh-my-posh --init --shell fish --config ~/.poshthemes/sonicboom_light.omp.json | source
 
 
 # ALIASES
@@ -33,23 +31,29 @@ alias 	gh	   "cd ~/"
 alias 	codes  "cd ~/code"
 
 # Current project
-alias	app  "cd ~/code/p5/p5-bezier"	 	
-alias	chat "cd ~/code/reactjschat"
+alias	app		"cd ~/code/p5/p5-bezier"	 	
+alias	chat            "cd ~/code/reactjschat"	 	
 
 # Files  : Edit files
 alias   lvimrc   "cat ~/.vimrc"
-alias 	evimrc   "vim ~/.vimrc"
+alias 	evimrc   "nvim ~/.vimrc"
 alias 	lfish  "cat ~/.config/fish/config.fish"
-alias 	efish  "vim ~/.config/fish/config.fish"
+alias 	efish  "nvim ~/.config/fish/config.fish"
 
 # Git Token
-alias 	gittoken "echo git push https://danielplata79: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@github.com/danielplata79/reactjschat"
-# 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
+alias 	gittoken "echo git push https://danielplata79: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@github.com/danielplata79/reactjschat"
+
 # List Aliases
 
 # SHORTCUTS
 alias 	rr 	   "ranger" 
 
-# Screens
+# Display
+#alias d2on "xrandr --output DisplayPort-1 --mode 1920x1080 --pos 2560x0 --rotate normal --gamma 0.85:0.85:0.85 && qtile cmd-obj -o cmd -f restart && wal -i (cat ~/.cache/wal/wal) -a 80"
+
+alias d2on "xrandr --output DisplayPort-1 --mode 1920x1080 --pos 2560x0 --rotate normal --gamma 0.85:0.85:0.85 --rate 60.00 --output HDMI-A-0 --primary --mode 2560x1440 --rate 99.97 --pos 0x0 --rotate normal --gamma 0.75:0.75:0.75 && wal -i (cat ~/.cache/wal/wal) -a 80 && qtile cmd-obj -o cmd -f restart"
+
+alias d3 "xrandr --output HDMI-A-0 --primary --mode 2560x1440 --rate 99.97 --pos 0x0 --rotate normal --gamma 0.60:0.60:0.60"
+
+
 alias d2off "xrandr --output DisplayPort-1 --off"
-alias d2on "xrandr --output DisplayPort-1 --mode 1920x1080 --pos 2560x0 --rotate normal --gamma 0.85:0.85:0.85"
